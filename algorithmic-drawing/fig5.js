e8d07df1547d003d8797100c5904936ec0e10dbf
Elea <contact.elea.poirot@gmail.com>
2026-01-12 13:24:36 +0100

    ajout couleurs

--- algorithmic-drawing/fig5.js
+++ algorithmic-drawing/fig5.js
@@ -9,17 +9,20 @@ function draw() {
   background(500);
   const A = HEIGHT/2;
   const B = WIDTH/2;
-  for(let i = 1; i<2; i++) {
-    let r = HEIGHT*0.7;
-    for(let w = PI/4; w < 3.6; w+= 0.05) {
+  let r = HEIGHT*0.7;
+  let colorCount = 250;
+  for(let w = PI/4; w < 3.6; w+= 0.05) {
       let X = r*cos(w);
       let Y = r*sin(w);
+      
+      stroke(colorCount /2, colorCount / 3, colorCount);
       line(A+X, B-Y, A-Y, B-X);
       line(A-Y, B-X, A-X, B+X);
       line(A-X, B+Y, A+X, B-Y);
       line(A-X, B+Y, A+Y, B+X);
       line(A+Y, B+X, A+X, B-Y);
+      colorCount -=20;
+      
       r=r*0.94;
-    }
   }
 }
