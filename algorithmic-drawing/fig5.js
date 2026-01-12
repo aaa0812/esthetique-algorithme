fc076c3737c93ae6e278453e877ba6aaa20d6c11
Elea <contact.elea.poirot@gmail.com>
2026-01-12 15:11:45 +0100

    ajout de commentaires

--- algorithmic-drawing/fig5.js
+++ algorithmic-drawing/fig5.js
@@ -10,6 +10,7 @@ function draw() {
   const A = HEIGHT/2;
   const B = WIDTH/2;
   let r = HEIGHT*0.7;
+  //initialisation d'un compteur couleur pour changer la couleur des lignes dans la boucle
   let colorCount = 250;
   for(let w = PI/4; w < 3.6; w+= 0.05) {
       let X = r*cos(w);
@@ -21,6 +22,7 @@ function draw() {
       line(A-X, B+Y, A+X, B-Y);
       line(A-X, B+Y, A+Y, B+X);
       line(A+Y, B+X, A+X, B-Y);
+      //décrémentation du compteur couleur pour changer la couleur à la prochaine itération
       colorCount -=20;
       
       r=r*0.94;
