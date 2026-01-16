6020d9144844300a8f68b16ba06d236426422f4c
Elea <contact.elea.poirot@gmail.com>
2026-01-16 18:13:24 +0100

    couloir infini + lumières et textures

--- free/Assets/Scripts/Chunk.cs
+++ free/Assets/Scripts/Chunk.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Chunk : MonoBehaviour
 {
+    InputAction forward;
+    public float speed;
+    public float spawnDistance;
+    public GameObject chunk;
+    private bool hasChild = false;
+    private Vector3 displacement;
+    private float time = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        forward = InputSystem.actions.FindAction("Navigate/Forward");
+        displacement = new Vector3(0, 0, -7);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (forward.IsPressed())
+        {
+            StartCoroutine(MoveForward());
+        }
+    }
+
+    private IEnumerator MoveForward()
+    {
+        Vector3 startingPos = transform.position;
+        Vector3 finalPos = transform.position + (transform.forward * -2);
+
+        float elapsedTime = 0;
+
+        Vector3 pos = transform.position;
+
+        if (pos.z < -10)
+        {
+            Destroy(gameObject);
+        }
+
+        while (elapsedTime < time)
+        {
+            if (pos.z <= 0 && !hasChild)
+        {
+            hasChild = true;
+            Vector3 spawnPos = new Vector3(0, 0, 0);
+            spawnPos.z = pos.z + spawnDistance;
+            Instantiate(chunk, spawnPos, Quaternion.identity);
+        }
+
+            transform.position = Vector3.Lerp(startingPos, finalPos, elapsedTime / time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
