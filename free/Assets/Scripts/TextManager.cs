97864f916d24fd55a6ed4000b7f921c8f2a1c38e
Elea <contact.elea.poirot@gmail.com>
2026-01-17 14:50:04 +0100

    ajout de l'affichage des phrases

--- free/Assets/Scripts/TextManager.cs
+++ free/Assets/Scripts/TextManager.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class TextManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI textObj;
     Dictionary<int, string> texts = new Dictionary<int, string>{
     { 5, "Bonjour." },
     { 7, "Savez-vous pourquoi vous êtes ici ?" },
@@ -13,6 +15,14 @@ public class TextManager : MonoBehaviour
     { 13, "Je sais seulement que vous devriez arrêter d'avancer."},
     { 14, "C'est un conseil."},
     { 15, "J'insiste."},
+    { 20, "Ce n'est pas un jeu, vous devriez m'écouter."},
+    { 23, "Il est vrai que la patience est une vertue."},
+    { 24, "Mais on dit surtout que la curiosité est un vilain défaut."},
+    { 27, "Qu'est ce que vous pensez trouver au bout de ce couloir ?"},
+    { 28, "Quoi que ce soit, ce ne sera pas la solution."},
+    { 29, "Je vous l'assure."},
+    { 30, "Ce ne sera pas non plus quelque chose de positif."},
+    { 31, "Vous voyez bien qu'il n'y a pas de lumière."}
     };
 
     InputAction forward;
@@ -37,7 +47,7 @@ public class TextManager : MonoBehaviour
     {
         if (texts.ContainsKey(clickCount))
         {
-            Debug.Log(texts[clickCount]);
+           textObj.text = texts[clickCount];
         }
     }
 }
